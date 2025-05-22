cbbc396baf2b93b906a9736df7a31cd89a72b23e
MuhammadHanzala31 <156145091+MuhammadHanzala31@users.noreply.github.com>
2025-05-22 23:36:42 +0500

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:5001/api/v1",
+    baseURL : "https://mern-chat-app-e59o.onrender.com//api/v1",
     withCredentials : true
 })
